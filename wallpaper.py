d9418e29429f88ea8243bdcb543ff57da4ce6aa5
guiszk <guiszk@protonmail.com>
2021-02-07 13:23:46 -0300

    Changed --gradient option to --color

--- wallpaper.py
+++ wallpaper.py
@@ -6,13 +6,10 @@ import sys
 
 parser = argparse.ArgumentParser(description="Generate wallpapers.")
 parser.add_argument(dest="w", type=int, default=1920, help="Width")
-#parser.add_argument("-w", "--width", nargs=1, type=int, default=1920, help="Width")
 parser.add_argument(dest="e", type=int, help="Height")
-#parser.add_argument("-e", "--height", nargs=1, type=int, default=1080, help="Height")
 parser.add_argument('-q', '--sequence', type=int, nargs='?', default=False, help='Number of squares in sequence')
 parser.add_argument('-s', '--square', type=int, nargs='?', default=False, help='Number of squares in center')
-parser.add_argument('-g', '--gradient', type=str, nargs='?', const='blue', default=False, help='Make gradient (random color if not specified)')
-#parser.add_argument(dest="n", type=int, help="Square side size")
+parser.add_argument('-c', '--color', type=str, nargs='?', default=False, help='Use different shades of same color')
 args = parser.parse_args()
 
 def getcol():
@@ -31,28 +28,23 @@ def sequence(x):
         sstart = (square[0][0]+squarel*i, square[0][1]+squarel*i)
         send = (sstart[0]+squarel*i, sstart[1]+squarel*i)
         msquare = [sstart, send]
-        if(args.gradient):
-            fill = Color(args.gradient)
-            fill.luminance -= 0.08
-            fill = fill.hex
+        if(args.color):
+            fill = Color(args.color)
+            fill.luminance = random.randint(1, 9)/10
         else:
             fill = Color(pick_for=random.randrange(256))
         ImageDraw.Draw(img).rectangle(msquare, fill=fill.hex)
 def cont(x):
-    if(args.gradient):
-        fill = Color(args.gradient)
-        #grad = [i+(0.3/x) for i in range(1, x)]
-        grad = [(1/x)*i for i in range(1, x+1)]
-        print(grad)
+    if(args.color):
+        fill = Color(args.color)
     for i in range(x):
         for j in range(x):
             squarel = w*sperc*2/x
             sstart = (square[0][0]+squarel*j, square[0][1]+squarel*i)
             send = (square[0][0]+squarel*(j+1), square[0][1]+squarel*(i+1))
             msquare = [sstart, send]
-            if(args.gradient):
-                fill.luminance = grad[-j]
-                #fill = fill.hex
+            if(args.color):
+                fill.luminance = random.randint(1, 9)/10
             else:
                 fill = Color(pick_for=random.randrange(256))
             ImageDraw.Draw(img).rectangle(msquare, fill=fill.hex)
