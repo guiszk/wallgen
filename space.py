ece02cbe83ec3637d513c3def308cb5640288109
guiszk <guiszk@protonmail.com>
2021-03-11 23:00:27 -0300

    Added white color option

--- space.py
+++ space.py
@@ -3,15 +3,21 @@ import random
 import sys
 
 if(len(sys.argv) < 3):
-    print(f"{sys.argv[0]} <width> <height>")
+    print(f"{sys.argv[0]} <width> <height> <b|w>")
     sys.exit(1)
 
 w, h = int(sys.argv[1]), int(sys.argv[2])
 
-im = Image.new("RGB", (w, h), color=0)
+col = 0
+if(len(sys.argv) >= 4):
+    if(len(sys.argv[3]) == 1):
+        if(sys.argv[3] == 'w'):
+            col = 1
+
+im = Image.new("RGB", (w, h), color=(col*255, col*255, col*255))
 draw = ImageDraw.Draw(im)
 
-def stars():
+def stars(col):
     total = random.randint(500, 8000)
     print('Total stars:', total)
     rad = random.randint(1, 4)
@@ -20,12 +26,16 @@ def stars():
         x = random.randint(r, im.width-r)
         y = random.randint(r, im.height-r)
         points = (x-r, y-r, x+r, y+r)
-        minc = 180
-        maxc = 255
+        if(col):
+            minc = 0
+            maxc = 150
+        else:
+            minc = 180
+            maxc = 255
         fill = (random.randint(minc, maxc), random.randint(minc, maxc), random.randint(minc, maxc), random.randint(minc, maxc))
         draw.ellipse(points, fill=fill)
 
-def planets():
+def planets(col):
     total = random.randint(4, 10)
     maxw = int(im.width/total)
 
@@ -34,8 +44,14 @@ def planets():
     r = random.randint(100, maxw*5)
     y = int(im.height/2)
     points = (x-r, y-r, x+r, y+r)
-    draw.ellipse(points, fill=(0, 0, 0), outline=(255, 255, 255), width=4)
 
+    if(col):
+        fill = (255, 255, 255)
+        outline = (0, 0, 0)
+    else:
+        fill = (0, 0, 0)
+        outline = (255, 255, 255)
+    draw.ellipse(points, fill=fill, outline=outline, width=4)
 
     x += r + 100
     r = random.randint(10, maxw-100)
@@ -45,8 +61,14 @@ def planets():
         r = random.randint(20, maxw-100)
         y = int(im.height/2)
         points = (x-r, y-r, x+r, y+r)
-        draw.ellipse(points, fill=(0, 0, 0), outline=(255, 255, 255), width=4)
-stars()
-planets()
+        if(col):
+            fill = (255, 255, 255)
+            outline = (0, 0, 0)
+        else:
+            fill = (0, 0, 0)
+            outline = (255, 255, 255)
+        draw.ellipse(points, fill=fill, outline=outline, width=4)
+stars(col)
+planets(col)
 im.save('wallpapers/space.jpg')
 print("Image saved to wallpapers/space.jpg")
